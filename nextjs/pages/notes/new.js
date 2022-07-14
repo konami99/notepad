b27eca1d38dff1f59a55ee32df79adc8aa1fe416
Richard Chou <konami99@hotmail.com>
2022-07-14 21:59:11 +1000

    fake url

--- nextjs/pages/notes/new.js
+++ nextjs/pages/notes/new.js
@@ -36,7 +36,7 @@ export default function create() {
   const [postData, setPostData] = React.useState(null);
 
   const { data, error } = useSWR(
-    postData ? 'http://express-backend.prod.notepad.local/api/users' : null,
+    postData ? 'http://zexpress-backend.prod.notepad.local/api/users' : null,
     fetcher
   );
 

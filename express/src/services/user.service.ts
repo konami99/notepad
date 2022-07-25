41aaa9cfd4319441f432abc6220edbc419f60196
Richard Chou <konami99@hotmail.com>
2022-07-25 19:48:50 +1000

    added faktory

--- express/src/services/user.service.ts
+++ express/src/services/user.service.ts
@@ -1,10 +1,24 @@
 import { FilterQuery } from "mongoose";
 import { omit } from "lodash";
 import UserModel, { UserDocument, UserInput } from "../models/user.model";
+import faktory from "faktory-worker";
 
 export async function createUser({email, name}: UserInput) {
   try {
     const user = await UserModel.create({email, name});
+
+    
+
+    const client = await faktory.connect({
+      host: "faktory",
+      port: 7419,
+      password: '12345',
+      labels: [],
+    })
+    console.log('here');
+
+    await client.job("ResizeImage", { id: 399, size: "thumb" }).push();
+    await client.close();
     
     return user.toJSON();
   } catch (e: any) {

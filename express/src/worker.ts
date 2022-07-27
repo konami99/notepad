5580dfa9863f7bac4861ef657bceb87528cc32ec
Richard Chou <konami99@hotmail.com>
2022-07-27 21:53:40 +1000

    used env vars to get faktory credentials

--- express/src/worker.ts
+++ express/src/worker.ts
@@ -1,13 +1,13 @@
 import faktory, { JobFunction } from "faktory-worker";
 
 // @ts-ignore
-faktory.register("ResizeImage", async ({id, size}) => {
+faktory.register("ResizeImage", async ({ id, size }) => {
   console.log('processing');
   console.log(id);
   console.log(size);
 });
 
 faktory.work({
-  host: 'faktory',
-  password: '12345'
+  host: process.env.FAKTORY_HOST,
+  password: process.env.FAKTORY_PASSWORD
 });
